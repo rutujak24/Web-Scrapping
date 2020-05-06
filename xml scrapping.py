ff9be79583df47a223276827474393a430de0486
Rutuja Kawade <56619747+rutujak24@users.noreply.github.com>
2020-05-06 10:39:58 +0530

    Update xml scrapping.py

--- xml scrapping.py
+++ xml scrapping.py
@@ -2,8 +2,8 @@ import urllib.request as ur
 import xml.etree.ElementTree as et
 
 url = input('Enter location: ')
-#'http://py4e-data.dr-chuck.net/comments_458706.xml' actually used
-# 'http://python-data.dr-chuck.net/comments_42.xml' sample data
+# 'http://python-data.dr-chuck.net/comments_42.xml'
+# 'http://py4e-data.dr-chuck.net/comments_458706.xml'
 
 total_number = 0
 sum = 0
@@ -18,7 +18,7 @@ for count in counts:
     sum += int(count.text)
     total_number += 1
 
-print('Count:', total_number)
-print('Sum:', sum)
+print('Count:', total_number) #50
+print('Sum:', sum)  #2145
 
 
